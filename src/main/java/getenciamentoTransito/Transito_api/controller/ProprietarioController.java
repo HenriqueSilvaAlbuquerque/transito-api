3fd497a1ed8c92dff54b7192d5e3e1c142700fb7
HenriqueSilvaAlbuquerque <henrique.albuquerque345@gmail.com>
2026-08-15 15:00:31 -0300

    15/08/2026

--- src/main/java/getenciamentoTransito/Transito_api/controller/ProprietarioController.java
+++ src/main/java/getenciamentoTransito/Transito_api/controller/ProprietarioController.java
@@ -1,31 +1,23 @@
 package getenciamentoTransito.Transito_api.controller;
 
 import getenciamentoTransito.Transito_api.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
+    @PersistenceContext
+    private EntityManager manager;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar(){
-        var proprietario1=new Proprietario();
-        proprietario1.setId(1l);
-        proprietario1.setNome("caio");
-        proprietario1.setTelefone("1299784146");
-        proprietario1.setEmail("jouinazai@kia");
-
-        var proprietario2=new Proprietario();
-        proprietario2.setId(2l);
-        proprietario2.setNome("pedro");
-        proprietario2.setTelefone("7879789974115");
-        proprietario2.setEmail("fhghagaag@kia");
-
-        return Arrays.asList(proprietario1,proprietario2);
-
+        TypedQuery<Proprietario> query=manager.createQuery("from Proprietario",Proprietario.class);
+       return query.getResultList();
 
     }
 
--- src/main/java/getenciamentoTransito/Transito_api/domain/model/Proprietario.java
+++ src/main/java/getenciamentoTransito/Transito_api/domain/model/Proprietario.java
@@ -1,14 +1,17 @@
 package getenciamentoTransito.Transito_api.domain.model;
-
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 
+
 public class Proprietario {
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
